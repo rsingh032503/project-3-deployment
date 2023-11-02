885790585a452274d4fea51d5ac5abcd758e0c14
Rahul Singh <rahul_2003@tamu.edu>
2023-11-01 19:13:56 -0500

    basic setup

---
+++ project3/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sharetea</h1>
+        <h2>Menu</h2>
+      </header>
+      <section className="Menu Board">
+        {/* Menu items will go here */}
+      </section>
+    </div>
+  );
+}
+
+export default App;
