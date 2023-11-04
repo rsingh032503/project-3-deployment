b0f26284efd50adf98853d3fceba069f70f85897
Dnen14 <abhinav.nallam@gmail.com>
2023-11-04 18:37:59 -0500

    Added delete menu item button and added backend functionality for deleting, adding, and updating ingredients

--- client/src/pages/Manager.jsx
+++ client/src/pages/Manager.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import '../styles/Manager.css';
 
 function Manager() {
+  const [items, setItems] = useState([
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    // Add more items as needed
+  ]);
+
+  const handleMenuItemDelete = (name) => {
+    axios
+    .delete(`/menu_item/${name}`)
+    .then((response) => {
+      if (response.status === 200) {
+        // If the deletion is successful, update the state to reflect the changes
+        const updatedItems = items.filter(item => item.name !== name);
+        setItems(updatedItems);
+      } else {
+        console.error('Error deleting menu item');
+      }
+    })
+    .catch((error) => {
+      console.error('Error deleting menu item:', error);
+    });
+  };
+
+
   return (
     <div>
       <h2>Manager View</h2>
-      <p>Welcome, manager! Here's your view.</p>
+      <p></p>
+      <button className="deleteMenuItemButton" onClick={handleMenuItemDelete(items.name)}>Delete Menu Items</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -99,3 +99,56 @@ app.get('/menu_item_order_join_table', (req, res) => {
 app.get('/order_table', (req, res) => {
     getAllFromTable('order_table', res);
 });
+
+//Deletes a menu item based on the name that is provided
+app.delete("/menu_item/:name", async (req, res) =>{
+    try{
+        const {name} = req.params;
+        const deleteTodo = await pool.query("DELETE FROM menu_item WHERE name = $1", [
+            name
+        ]);
+        res.json("Menu Item was deleted!")
+    }catch(err){
+        console.log(err.message);
+    }
+});
+
+//Adds an ingredient to the data
+app.post('/ingredient', async (req, res) => {
+    try {
+      const { data_to_insert } = req.body; // Extract the data from the request body
+      const insertQuery = 'INSERT INTO ingredient VALUES ($1, $2, $3, $4)';
+      await pool.query(insertQuery, [data_to_insert.value1, data_to_insert.value2, data_to_insert.value3, data_to_insert.value4]);
+      res.json('Ingredient was added!');
+    } catch (err) {
+      console.error(err);
+      res.status(500).json('Error adding ingredient');
+    }
+  });
+
+//Deletes a ingredient based on the name that is provided
+app.delete("/ingredient/:name", async (req, res) =>{
+    try{
+        const {name} = req.params;
+        const deleteTodo = await pool.query("DELETE FROM ingredient WHERE name = $1", [
+            name
+        ]);
+        res.json("Ingredient was deleted!")
+    }catch(err){
+        console.log(err.message);
+    }
+});
+
+//Updates ingredients based on the name that is provided
+app.put('/ingredient/:name', async (req, res) => {
+    try {
+      const { name } = req.params; // Extract the ID from the URL
+      const { data_to_update } = req.body; // Extract the data to update from the request body
+      const updateQuery = 'UPDATE ingredient SET quantity = $1, restock_price = $2 WHERE name = $3';
+      await pool.query(updateQuery, [data_to_update.value1, data_to_update.value2, name]);
+      res.json('Ingredient was udpated!');
+    } catch (err) {
+      console.error(err);
+      res.status(500).json('Error updating ingredient');
+    }
+  });
