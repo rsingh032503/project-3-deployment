cf8584e308e993369539ce60918f7f80a9f12678
zakborman <zakborman@gmail.com>
2023-11-05 22:07:55 -0600

    Added buttons, list, and table to Manager GUI

--- client/src/pages/Manager.jsx
+++ client/src/pages/Manager.jsx
@@ -8,6 +8,10 @@ function Manager() {
     { id: 2, name: 'Item 2' },
     // Add more items as needed
   ]);
+  const [ingredients, setIngredients] = useState([
+    {id: 1, name: 'Ingredient 1', price: 0.49, quantity: 500},
+    {id: 2, name: 'Ingredient 2', price: 0.36, quantity: 750}
+  ])
 
   const handleMenuItemDelete = (name) => {
     axios
@@ -29,9 +33,56 @@ function Manager() {
 
   return (
     <div>
-      <h2>Manager View</h2>
-      <p></p>
-      <button className="deleteMenuItemButton" onClick={handleMenuItemDelete(items.name)}>Delete Menu Items</button>
+      <h2>Manager</h2>
+      <div className="ContentContainer">
+        <table className="ingredientsTable">
+          <thead>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Quantity</th>
+          </thead>
+          <tbody>
+            {ingredients.map((ingredient) => (
+              <tr key={ingredient.id}>
+                <td>{ingredient.name}</td>
+                <td>${ingredient.price}</td>
+                <td>{ingredient.quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        <div className="ButtonColumn">
+          <button>Restock Ingredient</button>
+          <button>Add Ingredient</button>
+          <button>Update Ingredient</button>
+          <button>Delete Ingredient</button>
+        </div>
+      </div>
+      
+      <div className="ContentContainer">
+        <div className="MenuItemsList">
+          <h3>Menu Items</h3>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="ButtonColumn">
+          <button>Add Menu Item</button>
+          <button>Update Menu Item</button>
+          <button onClick={handleMenuItemDelete(items.name)}>Delete Menu Items</button>
+        </div>
+      </div>
+
+      <div className="ReportButtons">
+        <button>Sales Report</button>
+        <button>Excess Report</button>
+        <button>Restock Report</button>
+      </div>
+      
     </div>
   );
 }
