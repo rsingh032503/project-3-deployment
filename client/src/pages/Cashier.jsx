8b2eb42a5d33253baefa1381fcdac60facee251e
Rahul Singh <rahul_2003@tamu.edu>
2023-11-06 00:39:01 -0600

    added some functionality on cashier view to add menu items to order summary

--- client/src/pages/Cashier.jsx
+++ client/src/pages/Cashier.jsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Cashier() {
+  const [menuItems, setMenuItems] = useState([]);
+  const [orderSummary, setOrderSummary] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/menu_item')
+      .then(response => response.json())
+      .then(data => setMenuItems(data.menu_item))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
+  function addToOrder(item) {
+    setOrderSummary(prevOrder => [...prevOrder, item]);
+  }
+
+  function removeFromOrder(index) {
+    setOrderSummary(prevOrder => prevOrder.filter((item, i) => i !== index));
+  }
+
   return (
     <div>
       <h2>Cashier View</h2>
-      <p>Welcome, cashier! Here's your view.</p>
+      <div>
+        <h3>Order Summary</h3>
+        {orderSummary.map((item, index) => (
+          <div key={index}>
+            <p>{item.name}${item.price}    
+              <button onClick={() => removeFromOrder(index)}>Remove</button>
+            </p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h3>Menu Items</h3>
+        {menuItems.map(item => (
+          <button key={item.id} onClick={() => addToOrder(item)}>
+            {item.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
