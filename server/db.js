6eefbf73ba8f3d1b7b85d27da6332b4146a4a74f
Rahul Singh <rahul_2003@tamu.edu>
2023-11-06 23:16:39 -0600

    fixed .env file not working; removed from repository

--- server/db.js
+++ server/db.js
@@ -1,24 +1,24 @@
-const Pool = require("pg").Pool
-const dotenv = require('dotenv').config();
+// const Pool = require("pg").Pool
+require('dotenv').config();
 
 
-
-// const pool = new Pool({
-//     user: process.env.PSQL_USER,
-//     host: process.env.PSQL_HOST,
-//     database: process.env.PSQL_DATABASE,
-//     password: process.env.PSQL_PASSWORD,
-//     port: parseInt(process.env.APP_PORT, 10),
-//     // ssl: {rejectUnauthorized: false}
-// });
 
 const pool = new Pool({
-    user: "csce315_909_rahul_2003",
-    host: "csce-315-db.engr.tamu.edu",
-    database: "csce315331_09m_db",
-    password: "Rs03252003#",
-    port: 5432,
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: parseInt(process.env.APP_PORT, 10),
     // ssl: {rejectUnauthorized: false}
 });
+
+// const pool = new Pool({
+//     user: "csce315_909_rahul_2003",
+//     host: "csce-315-db.engr.tamu.edu",
+//     database: "csce315331_09m_db",
+//     password: "Rs03252003#",
+//     port: 5432,
+//     // ssl: {rejectUnauthorized: false}
+// });
 
 module.exports = pool;
