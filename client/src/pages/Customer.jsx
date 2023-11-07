793f91beafe0bf9e0b9f0402f80f34a76e081017
bat2492 <bat2492@tamu.edu>
2023-11-07 13:16:59 -0600

    added rudamentary view for customer

--- client/src/pages/Customer.jsx
+++ client/src/pages/Customer.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Customer.css';
+import { images } from '../images.js';
 
 function Customer() {
+    const [menuItems, setMenuItems] = useState([]);
+    const [orderSummary, setOrderSummary] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://localhost:3000/menu_item')
+        .then(response => response.json())
+        .then(data => setMenuItems(data.menu_item))
+        .catch(error => console.error('Error:', error));
+    }, []);
+
   return (
     <div>
       <h2>Customer View</h2>
-      <p>Welcome, customer! Here's your view.</p>
+      <h3>Drinks:</h3>
+      <div className="grid">
+        {menuItems.map((item,index) => (
+            <a key={item.id} href=""> 
+                <button className="item" href="">
+                    <img src={images[item.name]} alt={item.name + " image"}/>
+                    <p>{item.name}</p>
+                    <p>${item.price}</p>
+                </button>
+            </a>
+        ))}
+      </div>
     </div>
   );
 }
