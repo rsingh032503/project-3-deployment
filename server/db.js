190c2c024575772a113889dfb04eea8b3368a538
Rahul Singh <rahul_2003@tamu.edu>
2023-11-06 23:18:17 -0600

    removed credentials from db.js

--- server/db.js
+++ server/db.js
@@ -12,13 +12,4 @@ const pool = new Pool({
     // ssl: {rejectUnauthorized: false}
 });
 
-// const pool = new Pool({
-//     user: "csce315_909_rahul_2003",
-//     host: "csce-315-db.engr.tamu.edu",
-//     database: "csce315331_09m_db",
-//     password: "Rs03252003#",
-//     port: 5432,
-//     // ssl: {rejectUnauthorized: false}
-// });
-
 module.exports = pool;
