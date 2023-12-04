5fd289cc985017ca4a7ad8eff9bd3024f01450d3
bat2492 <bat2492@tamu.edu>
2023-12-03 21:12:32 -0600

    added login functionality without user roles

--- client/src/router/UserTypeNavBar.jsx
+++ client/src/router/UserTypeNavBar.jsx
@@ -6,10 +6,11 @@ import { jwtDecode } from "jwt-decode";
 
 
 const UserTypeNavBar = () => {
+    console.log(import.meta);
     let location = useLocation();
     //console.log(location.pathname === "/");
 
-    const [user,changeUserState] = useState(undefined);
+    const [user,changeUserState] = useState(null);
     const [loggedIn,changeLoginState] = useState(false);
 
     const loginSucess = async (res) => {
@@ -31,15 +32,31 @@ const UserTypeNavBar = () => {
 
     return (
         <div className="navbar">
-            <Link className={((location.pathname === "/")? "active":"" )} to="/">MenuBoard</Link>
-            <Link className={((location.pathname === "/customer")? "active":"" )} to="/customer">Customer</Link>
-            <Link className={((location.pathname === "/cashier")? "active":"" )} to="/cashier">Cashier</Link>
-            <Link className={((location.pathname === "/manager")? "active":"" )} to="/manager">Manager</Link>
-            <GoogleOAuthProvider clientId='1041949387108-2g75rqvqqc2tt19pp2c884g7gqptgnpf.apps.googleusercontent.com'>
-                <GoogleLogin
-                    onSuccess={loginSucess}
-                    onError={loginFailure}
-                />
+            
+            
+            <GoogleOAuthProvider clientId={'1041949387108-2g75rqvqqc2tt19pp2c884g7gqptgnpf.apps.googleusercontent.com'}>
+                {user ? 
+                    (
+                        <div className='navbar'>
+                            <Link className={((location.pathname === "/")? "active":"" )} to="/">MenuBoard</Link>
+                            <Link className={((location.pathname === "/customer")? "active":"" )} to="/customer">Customer</Link>
+                            <Link className={((location.pathname === "/cashier")? "active":"" )} to="/cashier">Cashier</Link>
+                            <Link className={((location.pathname === "/manager")? "active":"" )} to="/manager">Manager</Link>
+                            <img className='userPicture' src={user.picture}></img>
+                        </div>
+                        
+                    ) 
+                    
+                    :
+                    <div className='navbar'>
+                        <Link className={((location.pathname === "/")? "active":"" )} to="/">MenuBoard</Link>
+                        <Link className={((location.pathname === "/customer")? "active":"" )} to="/customer">Customer</Link>
+                        <GoogleLogin
+                        onSuccess={loginSucess}
+                        onError={loginFailure}
+                        />
+                    </div>
+                }
             </GoogleOAuthProvider>
         </div>
      );
